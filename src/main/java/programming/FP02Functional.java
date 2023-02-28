60cfed2add51c8ff399462e1ee2fdbc4dbaca5a8
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2023-02-27 22:26:05 -0600

    maps, filter, reduce using stream()

---
+++ src/main/java/programming/FP02Functional.java
@@ -0,0 +1,46 @@
+package programming;
+
+import java.util.List;
+
+public class FP02Functional {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+/*
+        0 12
+        12 9
+        21 13
+        34 4
+        38 6
+        44 2
+        46 4
+        50 12
+        62 15
+        77
+*/
+
+//        jshell> numbers.stream().reduce(0, (x,y)-> x>y? x:y) if x is greater that y return x if not return y
+
+        int sum = addListFunctional(numbers);
+        System.out.println(sum);
+    }
+
+    private static int sum(int aggregate, int nextNumber) {
+        System.out.println(aggregate + " " + nextNumber);
+        return aggregate + nextNumber;
+    }
+
+    private static int addListFunctional(List<Integer> numbers) {
+        return numbers.stream()
+//                .reduce(0, FP02Functional::sum);
+//                .reduce(0, (x, y) -> x +y); lambda expression
+                .reduce(0, Integer:: sum); // method reference
+
+                //Combine them into one result => One Value
+                // 0 and FP02Functional::sum
+        }
+
+    }
+
+
